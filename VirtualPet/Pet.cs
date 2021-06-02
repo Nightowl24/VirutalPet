dde97c8301e6de003bc376ea6cc6034cb91c0693
SymoneSays83 <S01198568@acad.tri-c.edu>
2021-06-02 12:04:40 -0400

    Added tests to boredom

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -8,21 +8,56 @@ namespace VirtualPet
     {
         public string Name { get; set; }
         public string Species { get; set; }
-        public string Hunger { get; set; }
+        public int Hunger { get; set; }
 
-        public string Boredom { get; set; }
-        public string Health { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
 
         public Pet()
         {
-            Name = "Pet Name";
-
-
+            Name = "";
+            Species = "";
         }
-        public void SetName(string petName) 
+
+        public void SetName(string name) 
         {
-            Name = petName;
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetSpecies(string species)
+        {
+            Species = species;
+        }
+
+        public string GetSpecies()
+        {
+            return Species;
         }
         
+        public void SetHunger(int hunger)
+        {
+            Hunger = hunger;
+        }
+
+        public int GetHunger()
+        {
+            Hunger = 50;
+            return Hunger;
+        }
+        public void SetBoredom(int boredom)
+        {
+            Boredom = boredom;
+        }
+
+        public int GetBoredom()
+        {
+            Boredom = 60;
+            return Boredom;
+        }
     }
 }
